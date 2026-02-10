4890b3ebf9ac87b2054cedac263413824b9f93c4
Dee Carreon <dcarreonr@gmail.com>
2026-02-10 00:28:14 -0600

    Created player state machine class

---
+++ scripts/character_blueprint/player_state_machine.gd
@@ -0,0 +1,9 @@
+class_name PlayerStateMachine
+extends StateMachine
+
+#const state_inherited_class_type := preload("res://directory/of/class.gd")
+#@export var inherited_class : state_inherited_class_type
+
+func _ready() -> void:
+	#_change_state(inherited_class)
+	pass
