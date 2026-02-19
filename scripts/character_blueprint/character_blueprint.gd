5ee545fcd3c7bf54e0e39c8bf16079678283f782
Dee Carreon <dcarreonr@gmail.com>
2026-02-18 21:49:29 -0600

    Horizontal movement and collision

--- scripts/character_blueprint/character_blueprint.gd
+++ scripts/character_blueprint/character_blueprint.gd
@@ -13,31 +13,37 @@ func _ready() -> void:
 
 
 func _get_local_input() -> Dictionary:
-	var input_vector := Input.get_vector(input_prefix + "left", input_prefix + "right", input_prefix + "up", input_prefix + "down")
+	var directional_input_vector := Input.get_vector(input_prefix + "left", input_prefix + "right", input_prefix + "up", input_prefix + "down")
 	var input : Dictionary
 	
-	if input_vector != Vector2.ZERO:
-		input["input_vector"] = input_vector
-#	if Input.is_action_just_pressed(input_prefix + "button_press"): 
-#		input["button_press"] = true
+	if directional_input_vector != Vector2.ZERO:
+		input["directional_input_vector"] = directional_input_vector
+	if Input.is_action_just_pressed(input_prefix + "light"):
+		input["light"] = true
 	
 	return input
 
 
-#func _predict_remote_input(previous_input: Dictionary, ticks_since_real_input: int) -> Dictionary:
-#	var input : Dictionary
-	#input = previous_input.duplicate()
-	#input.erase("input")
-#	return input
+func _predict_remote_input(previous_input: Dictionary, ticks_since_real_input: int) -> Dictionary:
+	var input := previous_input.duplicate()
+	input.erase("light")
+	return input
 
 
 func _network_process(input : Dictionary) -> void: # _process and _physics_process replacement
-	position += input.get("input_vector", Vector2.ZERO) * 8
+	var directional_input_vector : Vector2 = input.get("directional_input_vector", Vector2.ZERO)
+	
+	position.x += directional_input_vector.x * 8
+	
+	if input.get("light", false):
+		print("light pressed")
+		
+	move_and_slide()
 
 
 func _save_state() -> Dictionary:
 	return {
-		position = position
+		"position" : position
 	}
 
 
